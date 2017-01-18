667ce772225d608ca97db2fd5252ecf13a0886db
joshas <joshas@gmail.com>
2017-01-18 20:32:15 +0200

    First working version

---
+++ README.md
@@ -0,0 +1,8 @@
+# ExplorerProperties
+[fman](https://fman.io) plugin for viewing file details in Windows Explorer properties dialog. Works only on Windows OS. If anyone has ideas, how implement similar functionality on Linux and macOS - post it in new issue or create a pull request.
+
+## Usage
+ * `ALT+Enter` show properties for file or directory under cursor. Works with drives too.
+
+## Installation
+Download and extract it to `~/Library/Application Support/fman/Plugins`. Restart fman.
---
+++ explorer_properties/__init__.py
@@ -0,0 +1,42 @@
+from fman import DirectoryPaneCommand
+import os
+import ctypes
+import ctypes.wintypes
+
+SEE_MASK_NOCLOSEPROCESS = 0x00000040
+SEE_MASK_INVOKEIDLIST = 0x0000000C
+
+class SHELLEXECUTEINFO(ctypes.Structure):
+    _fields_ = (
+        ("cbSize",ctypes.wintypes.DWORD),
+        ("fMask",ctypes.c_ulong),
+        ("hwnd",ctypes.wintypes.HANDLE),
+        ("lpVerb",ctypes.c_char_p),
+        ("lpFile",ctypes.c_char_p),
+        ("lpParameters",ctypes.c_char_p),
+        ("lpDirectory",ctypes.c_char_p),
+        ("nShow",ctypes.c_int),
+        ("hInstApp",ctypes.wintypes.HINSTANCE),
+        ("lpIDList",ctypes.c_void_p),
+        ("lpClass",ctypes.c_char_p),
+        ("hKeyClass",ctypes.wintypes.HKEY),
+        ("dwHotKey",ctypes.wintypes.DWORD),
+        ("hIconOrMonitor",ctypes.wintypes.HANDLE),
+        ("hProcess",ctypes.wintypes.HANDLE),
+)
+
+class DisplayExplorerProperties(DirectoryPaneCommand):
+    def __call__(self):
+        file_name = self.pane.get_file_under_cursor()
+        
+        ShellExecuteEx = ctypes.windll.shell32.ShellExecuteEx
+        ShellExecuteEx.restype = ctypes.wintypes.BOOL
+        
+        sei = SHELLEXECUTEINFO()
+        sei.cbSize = ctypes.sizeof(sei)
+        sei.fMask = SEE_MASK_NOCLOSEPROCESS | SEE_MASK_INVOKEIDLIST
+        sei.lpVerb = str.encode("properties")
+        sei.lpFile = str.encode(file_name)
+        sei.nShow = 1
+        ShellExecuteEx(ctypes.byref(sei))
+        
